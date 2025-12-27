b1d7ada690ea29d2678df6bf7a0a607179924795
rezaelahidev <rezaelahi.dev@gmail.com>
2025-12-27 10:41:07 +0330

    Removed the unused files

--- plugin-a/packages/name-utils/php/functions.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-if (! function_exists('plugin_a_print_name')) {
-    function plugin_a_print_name(string $name): void {
-        echo 'Hello, ' . esc_html($name) . '!';
-    }
-}
--- plugin-b/packages/name-utils/php/functions.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if (! function_exists('plugin_b_print_name')) {
-    function plugin_b_print_name(string $name): void {
-        echo 'Hello, ' . esc_html($name) . '!';
-    }
-}
-
